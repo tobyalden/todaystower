ec5b20724e7211a4db4d91f0b8604f417310a001
Toby Alden <toby.alden@gmail.com>
2019-03-23 15:15:24 -0700

    Rearrange project

--- source/Main.hx
+++ source/Main.hx
@@ -1,13 +1,11 @@
-package;
-
 import flixel.FlxGame;
 import openfl.display.Sprite;
+import states.*;
 
 class Main extends Sprite
 {
-	public function new()
-	{
-		super();
-		addChild(new FlxGame(0, 0, PlayState));
-	}
+    public function new() {
+        super();
+        addChild(new FlxGame(0, 0, PlayState));
+    }
 }
